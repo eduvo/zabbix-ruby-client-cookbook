c3890993bd4bbafaf150abd1499d9425574fdfed
mose <mose@mose.com>
2014-08-24 02:39:04 +0800

    trying the version trick from gemspec in here, to use only changelog as a version indicator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,10 +3,13 @@ Zabbix Ruby Client Cookbook CHANGELOG
 
 This file is used to list changes made in each version of the zabbix-ruby-client cookbook.
 
+## 0.0.9 - wip
+
 ## 0.0.8 - 2014-08-23
 - [mose] write tests using kitchen-docker
 - [mose] make tests work on mac when using docker-osx
 - [mose] refactor for centos compatibility
+- [mose] first publication on the Supermarket
 
 ## 0.0.1 - 2014-08-20
 - [mose] Initial preparation of the cookbook
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mose@mose.com'
 license          'MIT'
 description      'Installs/Configures Zabbix-ruby-client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
+version          IO.read(File.expand_path('../CHANGELOG.md', __FILE__))[/([0-9]+\.[0-9]+\.[0-9]+)/]
 
 depends 'logrotate',   '~> 1.6.0'  # https://supermarket.getchef.com/cookbooks/logrotate
 depends 'git',         '~> 4.0.2'  # https://supermarket.getchef.com/cookbooks/git
